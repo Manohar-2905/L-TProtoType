f4e55f48dc1e2a386f265658f6d5a5695a1f4f4d
manohar-2905 <kumarmanohar6206@gmail.com>
2026-02-28 01:14:05 +0530

    fix: use direct regex for catch-all route for Express 5

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ setInterval(() => {
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // SPA Catch-all
-app.get('(.*)', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
